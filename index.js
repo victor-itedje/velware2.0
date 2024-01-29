76a8402903c59d17729c356e0cfa690beb66f963
victor-itedje <itedje309@gmail.com>
2024-01-29 17:22:54 +0100

    scroll effect

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-
-
-
-
 function myFunction(x) {
   x.classList.toggle("change");
 }
 
 let respNavbar = document.getElementById("resp");
-let menu = document.getElementById("hamburgercon")
+let menu = document.getElementById("hamburgercon");
 
 function navBar() {
-    if (respNavbar.style.height === "250px") {
-      respNavbar.style.height = "0px";
-      respNavbar.style.padding = "0px"
-    } else {
-      respNavbar.style.height = "250px";
-      respNavbar.style.padding = "20px"
-    }
-
+  if (respNavbar.style.height === "250px") {
+    respNavbar.style.height = "0px";
+    respNavbar.style.padding = "0px";
+  } else {
+    respNavbar.style.height = "250px";
+    respNavbar.style.padding = "20px";
+  }
 }
 
-menu.addEventListener("click", navBar)
+menu.addEventListener("click", navBar);
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);

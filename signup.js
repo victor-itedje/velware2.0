bb2f8b7d982ce7b251834dfa268547662944d08b
victor-itedje <itedje309@gmail.com>
2024-02-10 18:01:56 +0100

    jh

--- signup.js
+++ signup.js
@@ -13,17 +13,27 @@ passwordToggle.addEventListener("click", passwordVisible);
 
 var userDate = new Date();
 var currentHour = userDate.getHours();
+var i = 0;
+var txt1 = "Bonjour!, Good Morning, Ready to start your day with us?😃";
+var txt2 = "Bon Apres-Midi!, Good Afternoon, Refresh your day with us?😃";
+var txt3 = "Bon soiree!, Good Evening, Unwind your day with us?😃";
+var speed = 70;
 
 function callDate() {
-  if (currentHour >= 0 && currentHour <= 12) {
-    document.getElementById("hour").innerHTML =
-      "Bonjour!, Good Morning, Ready to start your day with us?";
-  } else if (currentHour >= 12 && currentHour < 17) {
-    document.getElementById("hour").innerHTML =
-      "Bon Apres-Midi!, Good Afternoon, Refresh your day with us?";
+  if (currentHour >= 0 && currentHour <= 12 && i < txt1.length) {
+    document.getElementById("hour").innerHTML += txt1.charAt(i);
+    i++;
+    setTimeout(callDate, speed)
+  } else if (currentHour >= 12 && currentHour < 17 && txt2.length) {
+    document.getElementById("hour").innerHTML +=
+      txt2.charAt(i);
+      i++;
+      setTimeout(callDate, speed)
   } else {
-    document.getElementById("hour").innerHTML =
-      "Bon soiree!, Good Evening, Unwind your day with us?";
+    document.getElementById("hour").innerHTML +=
+      txt3.charAt(i);
+      i++;
+      setTimeout(callDate, speed)
   }
 }
 

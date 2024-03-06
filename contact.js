4d0c09b71df9660f97274b9123116fc1422d408f
victor-itedje <itedje309@gmail.com>
2024-03-06 13:45:22 +0100

    profile

--- contact.js
+++ contact.js
@@ -1,21 +1,3 @@
-var speed = 100;
-var txt4 = "Services";
-var i = 0;
-
-function servicesType() {
-    if(i < txt4.length) {
-         document.getElementById("bigservices").innerHTML += txt4.charAt(i);
-    i++;
-    setTimeout(servicesType, speed)
-    }else {
-         document.getElementById("bigservices").innerHTML += txt4.charAt(i);
-         i++;
-         setTimeout(servicesType, speed);
-    }
-}
-
-servicesType();
-
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
